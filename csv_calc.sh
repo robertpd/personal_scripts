b8dd33db017bf43729e965c80e4e38d3bfe19dca
rob <robertofdooley@gmail.com>
2019-05-14 23:23:08 -0400

    Parse decimals in sort

--- csv_calc.sh
+++ csv_calc.sh
@@ -2,13 +2,13 @@
 
 ACCUMULATOR=()
 
-YEAR=2019
+YEAR=2015
 echo $YEAR
 for filename in *$YEAR*.csv; do
-	ACCUMULATOR+=(`cut -d ',' -f5 $filename  | sort -r | head -n 1`)
+	ACCUMULATOR+=(`cut -d ',' -f5 $filename  | sort --reverse --numeric-sort | head -n 1`)
 done
 
-IFS=$'\n' sorted=($(sort -r <<<"${ACCUMULATOR[*]}"))
+IFS=$'\n' sorted=($(sort --reverse --numeric-sort <<<"${ACCUMULATOR[*]}"))
 unset IFS
 
 echo $sorted
